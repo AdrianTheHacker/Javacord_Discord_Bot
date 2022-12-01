869e573f72d024ca56fef89c7ae535a1f8899451
AdrianTheHacker <adriantarantino2006@gmail.com>
2022-11-30 19:55:28 -0500

    Adding Locally Hosted Code into Repo

---
+++ app/src/main/java/java_discord_bot/discord_bot/DiscordBot.java
@@ -0,0 +1,33 @@
+package java_discord_bot.discord_bot;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.intent.Intent;
+
+
+public class DiscordBot {
+    private DiscordApi api;
+
+    
+    public DiscordBot(String discordBotToken) {
+        this.api = new DiscordApiBuilder()
+            .setToken(discordBotToken)     // "MTAxODY0MDgxMTI0MTUyNTMxMQ.GjKqY3.tPbULD8g8VF3IcP5cnP9Tr7smweCopy0A1boyM"
+            .addIntents(Intent.MESSAGE_CONTENT)
+            .addIntents(Intent.GUILD_EMOJIS)
+            .login()
+            .join();
+    }
+
+    private void eventListener() {
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        this.api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("Who is Adrian")) {
+                event.getChannel().sendMessage("Adrian is :Chadrian: ");
+            }
+        });
+    }
+
+    public void run() {
+        eventListener();
+    }
+}
